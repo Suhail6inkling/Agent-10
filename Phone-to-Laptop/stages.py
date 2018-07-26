2e65e1cf8e64d7aa6b35806e1c4ed0a761ab5531
Suhail6inkling <38522108+Suhail6inkling@users.noreply.github.com>
2018-07-26 04:21:33 +0100

    Update stages.py

--- Phone-to-Laptop/stages.py
+++ Phone-to-Laptop/stages.py
@@ -1,1 +1,4 @@
-print("Test")
+async def stages(self, ctx, *, nothing = None, where: discord.TextChannel):
+    #get regular, ranked and league
+    regular_embed = discord.Embed(name = "Regular Battle",colour=0x000000)
+    regular_embed.add_field(title="Now",content=f"{regular[0]["stage_a"]}\n{regular[0]["stage_b"]}")
